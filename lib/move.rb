191bba7810c657a3cb3435b2d9e1e766b008ab20
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-04 18:11:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,7 +15,8 @@ board = [" ", " ", " ",
         ]
 def move(array, index, character="X")
 array[index] = character
-board = ["#{array[0]}", "#{array[1]}", "#{array[2]}",
+array = ["#{array[0]}", "#{array[1]}", "#{array[2]}",
          "#{array[3]}", "#{array[4]}", "#{array[5]}",
          "#{array[6]}", "#{array[7]}", "#{array[8]}", ]
+
 end
