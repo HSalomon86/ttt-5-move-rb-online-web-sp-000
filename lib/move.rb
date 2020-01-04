1bd77aedbabc2541eb67f3a531d2eca66eaea7d7
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-04 17:48:20 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,5 +11,7 @@ num = num.to_i - 1
 end
 
 def move(array, index, character="X")
-
+board = ["#{array[0]}", "#{array[1]}", "#{array[2]}",
+         "#{array[3]}", "#{array[4]}", "#{array[5]}",
+         "#{array[6]}", "#{array[7]}", "#{array[8]}", ]
 end
